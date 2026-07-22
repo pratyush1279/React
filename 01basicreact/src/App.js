0ada9ae648297d9ffd74a212ec10dbabab1d0284
pratyush1279 <pratyushpatwal@gmail.com>
2026-07-22 16:22:17 +0530

    new project

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Hello World | Pratyush Patwal | This is very interesting course for beginners where you will learn lots of things.</h1>
+  );
+}
+
+export default App;
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Hello World with Vite | Pratyush Patwal</h1>
+  )
+}
+
+export default App
