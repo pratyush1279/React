a0159bce2396e5f50ed489bd344c96cb95a63fdf
pratyush1279 <pratyushpatwal@gmail.com>
2026-07-29 10:28:10 +0530

    new code

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,11 +3,11 @@
 import Chai from "./chai"
 
 function App() {
-  
+  const username = "hello"
 
   return (
     <>
-    <h1>Hello World with Vite | Pratyush Patwal</h1>
+    <h1>Hello World with Vite {username} | Pratyush Patwal</h1>
     <p>HELLO! Everyone</p>
     </>
   )
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,42 @@
+import React from "react";
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  
-    <App />
-  
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App !</h1>
+        </div>
+    )
+}
+
+// const ReactElement = {   // this is not a correct syntax 
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (  // this is correct syntax (js fundamentals)
+    <a href="https://google.com" target = '_blank'>Visit google </a>
+)
+
+// building according to react
+
+const anotherUser = "chai aur react"
+
+const reactElement = React.createElement(  // babble will inject
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'Click me to visit google',
+    anotherUser
+)
+
+createRoot(document.getElementById("root")).render(
+    reactElement
 )
